27d54833d21f771a778c48fd2d155028017c9c3f
PhamTrHung <phamhung2002ph@gmail.com>
2024-08-01 12:53:26 +0700

    all

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FindNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tbLoutNum = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStart.Location = new System.Drawing.Point(600, 237);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.TabStop = false;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(682, 237);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(75, 23);
+            this.btnFinish.TabIndex = 3;
+            this.btnFinish.TabStop = false;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.btnFinish_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.BackColor = System.Drawing.Color.Yellow;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(601, 277);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblTime.Size = new System.Drawing.Size(153, 76);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "180";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbLoutNum
+            // 
+            this.tbLoutNum.ColumnCount = 8;
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tbLoutNum.Location = new System.Drawing.Point(0, 0);
+            this.tbLoutNum.Name = "tbLoutNum";
+            this.tbLoutNum.RowCount = 8;
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tbLoutNum.Size = new System.Drawing.Size(570, 391);
+            this.tbLoutNum.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = global::FindNumber.Properties.Resources.tim_so_201;
+            this.label1.Location = new System.Drawing.Point(592, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 121);
+            this.label1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::FindNumber.Properties.Resources.eavesdropping;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(625, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 86);
+            this.button1.TabIndex = 8;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(640, 365);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(766, 391);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLoutNum);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TableLayoutPanel tbLoutNum;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Media;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace FindNumber
+{
+    public partial class Form1 : Form
+    {
+        Random ran = new Random();
+        int numText;
+        int indNum = 0;
+        ArrayList numCorrect = new ArrayList(64);
+        int playTime = 220;
+        int score = 0;
+        ArrayList indArr = new ArrayList(64);
+        SoundPlayer saoCorrect = new SoundPlayer("..\\..\\floop2_x.wav");
+        SoundPlayer saoFault = new SoundPlayer("..\\..\\blurp_x.wav");
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        bool isStart = false;
+        Font f = new System.Drawing.Font("Arial", 10f, FontStyle.Bold);
+
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 64; i++)
+            {
+                indArr.Add(i);
+            }
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Volume = 50;
+            synth.Rate = 1;
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            saoCorrect.Play();
+            isStart = true;
+            btnExit.Enabled = false;
+
+            for (int i = 0; i < 64; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 65;
+                btn.Height = 42;
+                btn.TabStop = false;
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.MouseOverBackColor = Color.Red;
+                btn.FlatAppearance.BorderColor = Color.Black;
+
+                numText = ran.Next(0, 100);
+                numCorrect.Add(numText);
+                btn.Text = numText.ToString();
+                btn.Click += BtnNum_Click;
+                btn.Font = f;
+
+                tbLoutNum.Controls.Add(btn);
+            }
+
+            indNum = ran.Next(numCorrect.Count);
+            synth.SpeakAsync(numCorrect[indNum].ToString());
+
+            btnStart.Enabled = false;
+        }
+
+        private void BtnNum_Click(object sender, EventArgs e)
+        {
+            Button btNum = sender as Button;
+            
+            if(Convert.ToInt32(btNum.Text) == (int)numCorrect[indNum])
+            {
+                saoCorrect.Play();
+
+                Console.WriteLine(numCorrect.Count.ToString());
+                score++;
+                btNum.Enabled = false;
+                if (numCorrect.Count == 1) return;
+                numCorrect.Remove(numCorrect[indNum]);
+                indNum = ran.Next(numCorrect.Count);
+                Thread.Sleep(150);
+                synth.SpeakAsync(numCorrect[indNum].ToString());
+            }
+            else
+            {
+                saoFault.Play();
+            }
+        }
+
+        private void btnFinish_Click(object sender, EventArgs e)
+        {
+            isStart = false;
+            timer1.Enabled=false;
+
+            btnExit.Enabled=true;
+            btnStart.Enabled = true;
+            tbLoutNum.Controls.Clear();
+            playTime = 220;
+            numCorrect.Clear();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            playTime--;
+            if (playTime == 0) btnFinish.PerformClick();
+            lblTime.Text = playTime.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            synth.SpeakAsync("A Ha Ha Ha");
+            if (isStart == true)
+                synth.SpeakAsync(numCorrect[indNum].ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Are you sure ???", "Exit", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+                e.Cancel = true;
+        }
+    }
+}
